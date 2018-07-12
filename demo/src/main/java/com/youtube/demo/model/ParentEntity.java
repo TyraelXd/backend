9eccee04129fcf4c461e917e2ad141415c709f1b
TyraelXd <pval_29@outlook.com>
2018-07-12 14:11:39 -0400

    Entity user created

---
+++ demo/src/main/java/com/youtube/demo/model/ParentEntity.java
@@ -0,0 +1,39 @@
+package com.youtube.demo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+
+@MappedSuperclass
+@Access(AccessType.FIELD)
+public class ParentEntity implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9022132952590357619L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "id", unique = true, nullable = false)
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+
+
+
+}
